101a3384344ed43e14680d5a976989004cae90aa
osman yaycioglu <osman.yaycioglu@gmail.com>
2024-06-03 11:01:09 +0300

    Initial commit

---
+++ src/test/java/org/training/spring/springtraining20240603/SpringTraining20240603ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.training.spring.springtraining20240603;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTraining20240603ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
